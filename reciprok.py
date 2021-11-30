64bc9fe238b7d3fca787e30c26b0c6e338cff987
Tanuló 301-03 <301-03@KKSZKI.LOCAL>
2021-11-30 18:53:00 +0100

    Kész a feladat

---
+++ reciprok.py
@@ -0,0 +1,27 @@
+#Első feladat
+#for i in range(1,11):
+    #print(i,"---",1/i)
+
+#Második feladat
+#a=int(input("Kérem a hatvány alapot:"))
+#b=int(input("Kérem a hatvány kitevőt:"))
+
+#hatvanyertek=a**b
+
+#print(hatvanyertek)
+
+#Harmadik feladat
+#while True:
+    #c=int(input("Kérem a számot: "))
+    #if c>=0:
+            #print(c)
+    #else:
+        #break
+#Negyedik feladat
+a=int (input("Kérem az első számot: "))
+b=int (input("Kérem a második számot: "))
+
+print(-(a-b))
+
+#Ötödik feladat
+
